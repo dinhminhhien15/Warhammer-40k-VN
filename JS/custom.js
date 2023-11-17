c8ee761442f90c3efe3f5635a7ce7f034a21abf0
dinhminhhien15 <142479480+dinhminhhien15@users.noreply.github.com>
2023-11-17 12:20:49 +0700

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,55 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#dichvu-chay').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:5000,
+	smartSpeed:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('#sanpham').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	autoplay:true,
+	autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
